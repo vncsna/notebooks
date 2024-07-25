48e450742c5395ca557f96e756453f7ad485b1bf
Vinicius Aguiar <vncsna@gmail.com>
2024-07-25 09:02:56 -0300

    feat: add in-place quick sort

--- algo/sort/quicksort.py
+++ algo/sort/quicksort.py
@@ -1,11 +1,35 @@
 from random import randint
 
-def quicksort(arr: list[int]):
+arr = [randint(0, 10) for i in range(10)]
+
+
+def quicksort1(arr: list[int]):
     if len(arr) <= 1:
         return arr
     left = [a for a in arr if a < arr[-1]]
     right = [a for a in arr if a > arr[-1]]
     middle = [a for a in arr if a == arr[-1]]
-    return quicksort(left) + middle + quicksort(right)
+    return quicksort1(left) + middle + quicksort1(right)
 
-quicksort([randint(0, 10) for i in range(10)])
+
+def quicksort2(arr, left, right):
+    if left < right:
+        pivot = partition2(arr, left, right)
+        quicksort2(arr, left, pivot - 1)
+        quicksort2(arr, pivot + 1, right)
+    return arr
+
+
+def partition2(arr, left, right):
+    i = left - 1
+    pivot = arr[right]
+    for j in range(left, right):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[right] = arr[right], arr[i + 1]
+    return i + 1
+
+
+print(quicksort1(arr))
+print(quicksort2(arr, 0, len(arr) - 1))
